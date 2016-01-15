684776e74a79dabe8c0c7346c107d2bd60db8882
Luiz Amorim <luiz.simples@gmail.com>
2016-01-15 20:11:29 -0200

    bugfix

--- api/src/MessageBoardServer.js
+++ api/src/MessageBoardServer.js
@@ -1,15 +1,17 @@
 'use strict';
 
+var path = require('path');
 var http = require('http');
 var express = require('express');
 var socketIO = require('socket.io');
 var machineIPs = require('./lib/machine.ips');
+var publicPath = path.dirname(__dirname).concat('/public');
 
 function MessageBoardServer() {
   var server = this;
 
   server.appExpress = express();
-  server.appExpress.use('/', express.static(__dirname.concat('/public')));
+  server.appExpress.use('/', express.static(publicPath));
 
   server.appHttp = http.Server(server.appExpress);
   server.appSocket = socketIO(server.appHttp);
